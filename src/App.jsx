901cc31f6c4e726691a9e8be74d1598de3f2d28d
Mohd Kamran Ahmed <ahmed22kamran@gmail.com>
2026-02-20 22:25:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="bg-red-500 text-2xl p-4 text-green-500 font-bold">Hello world!!!</h1>
+    </div>
+  )
+}
+
+export default App
